67cc07d4ba5f671d3680c36f5dfdd15a21cca8da
leejiwon <leejiwon2325@gmail.com>
2026-09-01 16:14:54 +0900

    Exclude multi-agent backup

--- schema linking/multi-agent backup/schema_agents/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Dependency-free multi-agent baseline for Spider-Ko Text-to-SQL."""
-
-from .agentic_orchestrator import (
-    AgenticMultiAgentSchemaLinker as MultiAgentSchemaLinker,
-)
-from .sql_orchestrator import MultiAgentSQLGenerator
-
-__all__ = ["MultiAgentSchemaLinker", "MultiAgentSQLGenerator"]
